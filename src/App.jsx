7604b513946948cb4d196cff1cb4dfd0cd8f6258
EetuPe <eetu.petanen@gmail.com>
2023-03-28 19:27:02 +0300

    toaster

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,24 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { toast, Toaster } from 'react-hot-toast';
+
 
 function App() {
   const [count, setCount] = useState(0)
 
+  function greeting() {
+    increment();
+    toast.success(`count is ${count}`)
+  }
+  
+  function increment() {
+    setCount(count + 1)
+  }
+
   return (
+    <>
+      <Toaster />
     <div className="App">
       <div>
         <a href="https://vitejs.dev" target="_blank">
@@ -18,8 +31,8 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <button onClick={() =>  greeting()}>
+          Count
         </button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
@@ -29,6 +42,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </div>
+    </>
   )
 }
 
